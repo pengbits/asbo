1eda2cfeb40a9cf53bffbabf23c7d953401abbfc
Dave Paul <david.paul@showtime.net>
2018-02-01 11:38:49 -0500

    hard-coded step

---
+++ features/feed_steps.rb
@@ -0,0 +1,17 @@
+
+Given("a platform with the nickname nts") do
+  puts "get platform 'nts'"
+end
+
+Given("the tag murlo") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I view the feed") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should get one or more episodes of a show on nts with murlo in it's name") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
