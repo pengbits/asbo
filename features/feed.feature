18262229c6c9ece6930108c1e80a16a97920254c
Dave Paul <david.paul@showtime.net>
2018-02-01 15:54:03 -0500

    api-centric JSON steps, passing

--- features/feed.feature
+++ features/feed.feature
@@ -2,8 +2,17 @@ Feature: View Feed
    In order to reduce a large collection of episodes to a manageable list
    I want to view a subset of them
    
-   
-Scenario: View Latest NTS episodes
-  Given The platform exists, and has some episodes
-  When I view the feed page for the platform
-  Then I should get one or more episodes of shows
+
+   Scenario: With a valid platform
+     Given the following platforms:
+        |name|key|
+        |nts.live|nts|
+        |rinse.fm|rinse|
+        |radarradio|radar|
+      When I load the platform endpoint with key: "nts"
+      Then The response should be a JSON representation of the platform
+           
+  # Scenario: View Latest NTS episodes
+    # Given The platform exists, and has some episodes
+     # When I view the feed page for the platform
+     # Then I should get one or more episodes of shows
--- features/feed_steps.rb
+++ features/feed_steps.rb
@@ -4,10 +4,30 @@ Given("The platform exists, and has some episodes") do
 end
 
 When("I view the feed page for the platform") do
-  visit "/platforms/#{@platform.key}"
+  get "/platforms/#{@platform.key}"
 end
 
 Then("I should get one or more episodes of shows") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(last_response.status).to be(204)
+  # expect(JSON.parse(last_response.body)).to eq(@platform.to_json)
+  # pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+Given("the following platforms:") do |platforms|
+  @platforms = Platform.create!(platforms.hashes)
+  expect(@platforms.length).to eq(platforms.hashes.length)
+end
+
+When("I load the platform endpoint with key: {string}") do |key|
+  get "/platforms/#{key}"
+  @params = {:key => key}
+  @response = last_response.body
+  
+end
+
+Then("The response should be a JSON representation of the platform") do
+  @platform = @platforms.find{|p| p.key == @params[:key] }
+  expect(@response).to eq(@platform.to_json)
 end
 
