e150d1a8483d6a1593679395f86a803fdd918a32
Dave Paul <david.paul@showtime.net>
2018-02-01 14:00:17 -0500

    platform x episode association + factory

--- features/feed_steps.rb
+++ features/feed_steps.rb
@@ -1,18 +1,16 @@
 
-Given(/^a platform with the nickname (.+)$/) do |key|
-  puts "get platform #{key}"
-  FactoryBot.create(:platform, key: name)
-end
-
-Given("the tag murlo") do
-  pending # Write code here that turns the phrase above into concrete actions
+Given("a platform") do
+  FactoryBot.create(:platform)
 end
 
 When("I view the feed") do
+  # p = FactoryBot.attributes_for(:platform)
+  @platform = FactoryBot.create(:platform_with_episodes, episodes_count: 5)
+  puts @platform.episodes.length
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then("I should get one or more episodes of a show on nts with murlo in it's name") do
+Then("I should get one or more episodes of shows for the platform") do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
