c31f9fa50e41213469f44905c08adfc726334f7c
Dave Paul <david.paul@showtime.net>
2018-02-01 14:06:57 -0500

    when i view w associations passes

--- features/feed_steps.rb
+++ features/feed_steps.rb
@@ -6,8 +6,7 @@ end
 When("I view the feed") do
   # p = FactoryBot.attributes_for(:platform)
   @platform = FactoryBot.create(:platform_with_episodes, episodes_count: 5)
-  puts @platform.episodes.length
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(@platform.episodes.length).to eq(5)
 end
 
 Then("I should get one or more episodes of shows for the platform") do
