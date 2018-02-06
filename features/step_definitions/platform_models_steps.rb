2d1856b906fd74dcf81ba180c41eb940421ccaee
Dave Paul <david.paul@showtime.net>
2018-02-06 11:37:24 -0500

    adding platform to db and reading back out with `platforms#index` passes

--- features/step_definitions/platform_models_steps.rb
+++ features/step_definitions/platform_models_steps.rb
@@ -1,7 +1,5 @@
 Given("these attributes") do |attrs|
   @platform_attrs = JSON.parse(attrs)
-  puts @platform_attrs
-  # pending # Write code here that turns the phrase above into concrete actions
 end
 
 When("I save it") do
@@ -11,10 +9,6 @@ end
 Then("I should see the new platform in the list") do
   get "/platforms"
   @response = last_response.body
-  puts "response"
-  puts @response
-  puts "model.to_json"
-  puts @platform.to_json
   expect(@response).to include(@platform.to_json)
 end
 
