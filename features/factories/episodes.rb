41a1278c5691a4891d1aa9ec7609afddc8dda0cb
Dave Paul <david.paul@showtime.net>
2018-02-06 11:38:27 -0500

    updated img to image in Episode factory

--- features/factories/episodes.rb
+++ features/factories/episodes.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do  
   factory :episode do
     name "murlo 31.01.18"
-    img "https://media.ntslive.co.uk/resize/800x800/24463c4a-6d6d-48d7-9026-62c9af3b996f_1456963200.jpeg"
+    image "https://media.ntslive.co.uk/resize/800x800/24463c4a-6d6d-48d7-9026-62c9af3b996f_1456963200.jpeg"
     platform
   end
 end
