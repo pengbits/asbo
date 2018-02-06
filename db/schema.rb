093f024c8575eb66cc939f58c13df71c3c12beda
Dave Paul <david.paul@showtime.net>
2018-02-06 11:07:50 -0500

    add attribute_map to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202211900) do
+ActiveRecord::Schema.define(version: 20180206151846) do
 
   create_table "episodes", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180202211900) do
     t.string "key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "attribute_map"
   end
 
 end
