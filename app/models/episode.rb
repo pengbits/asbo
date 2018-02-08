738115efa7e48a5e6ac4add8ae10854b6344dd3d
Dave Paul <david.paul@showtime.net>
2018-02-08 15:38:47 -0500

    setting date in arbitrary format results in date_proper saved to episode

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,7 +1,17 @@
 class Episode < ApplicationRecord
   belongs_to :platform
+  validates :date, presence: true
+  before_save :parse_date
   
   def as_json(opts={})
     super(opts.merge({:except => [:created_at,:updated_at]}))
+  end
+  
+  def parse_date
+    if platform && platform.date_format
+      self.date_proper = Date.strptime(self.date, platform.date_format)
+    else
+      raise "can't parse_date if date_format is not set on platform"
+    end
   end
 end
--- features/step_definitions/parse_dates_steps.rb
+++ features/step_definitions/parse_dates_steps.rb
@@ -7,25 +7,23 @@ Given("this date format {string}") do |f|
 end
 
 When("I set the date for the episode") do
-  @platform = Platform.new({
+  @platform = Platform.create({
     name:'nts.live', 
     url: "https://www.nts.live/recently-added",
     date_format: @date_format
   })
-  @platform.episodes.build({
+  @platform.episodes.create({
     name: "Ossia 06.02.18 Radio Episode",
     date: "06.02.18"
   })
-  
-  @platform.save
+
   @episode = @platform.episodes.first
-  
 end
 
 Then("the date_proper will be correct") do
-  puts @episode.date
-  puts @episode.date_proper
+  # puts @episode.date
+  # puts @episode.date_proper
   @date = Date.strptime(@episode.date, @platform.date_format)
-  puts @date
+  # puts @date
   expect(@date).to eq(@episode.date_proper)
 end
