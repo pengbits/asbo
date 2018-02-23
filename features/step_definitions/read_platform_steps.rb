a064006594ab582e6d6fac860a4961d92bb15f94
Dave Paul <david.paul@showtime.net>
2018-02-23 14:52:27 -0500

    cant seem to compare single record attrs without going insane

--- features/step_definitions/read_platform_steps.rb
+++ features/step_definitions/read_platform_steps.rb
@@ -1,16 +1,19 @@
 When("I load the platform endpoint {string}") do |url|
   get url
-  @response = last_response.body
+  @response = JSON.parse(last_response.body)
+  @key = url.split('/platforms/')[1]
 end
 
 Then("the response should be a JSON representation of the platform") do
-  puts last_response.params
-  @platform = @platforms.find{|p| p.key == @params[:key] }
-  # expect(@response).to eq(@platform.to_json)
+  @platform = @platforms.find{|p| p.key == @key }
+  expect(@platform.name).to eq(@response['name'])
+  expect(@platform.id).to   eq(@response['id'])
+  expect(@platform.key).to  eq(@response['key'])
+  expect(@platform.url).to  eq(@response['url'])
+  
 end
 
 Then("the response should be an error") do
-  @response = JSON.parse(last_response.body)
   expect(last_response.status).to eq(500)
   expect(@response).to eq({"error" => "Platform not found"})
 end
