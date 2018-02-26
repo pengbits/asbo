f36b3c926b4fed3f91f1baa1585dc487c95c8e88
Dave Paul <david.paul@showtime.net>
2018-02-26 14:15:34 -0500

    alls well now, but we need tests around changing models/out of date seeds

--- app/models/media.rb
+++ app/models/media.rb
@@ -6,6 +6,7 @@ class Media
   def self.from_url(url)
     trim = url.gsub(HTTP_TRIM_REGEX, '')
     type = TYPES_REGEX =~ trim ? trim[TYPES_REGEX] : 'unrecognized'
+    puts "Media::from_url type=#{type}"
     {type: type, url: url}
   end
 end
