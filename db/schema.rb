d44424ca676b4fca503bee8f47f7141e791189dc
Dave Paul <david.paul@showtime.net>
2018-02-27 13:34:38 -0500

    unpack media into two flat fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223212943) do
+ActiveRecord::Schema.define(version: 20180227165714) do
 
   create_table "episodes", force: :cascade do |t|
     t.string "name"
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 20180223212943) do
     t.string "image"
     t.date "date"
     t.string "details"
-    t.string "media"
+    t.string "media_type"
+    t.string "media_url"
   end
 
   create_table "platforms", force: :cascade do |t|
