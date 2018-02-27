8f0aca1ea2b473203fb18434ed340391f012b2ce
Dave Paul <david.paul@showtime.net>
2018-02-27 14:19:16 -0500

    added eps#index route, no controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
    get 'platforms/:key', to: 'platforms#show', as: 'platform'
   # this should prolly be PUT or POST since it creates episodes but platform exists
    get 'platforms/:key/refresh', to: 'platforms#refresh', as: 'platform_refresh'
+   
+   get 'episodes', to: 'episodes#index'
+
 end
