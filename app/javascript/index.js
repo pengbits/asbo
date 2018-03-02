71f05d686ae8b2c8da9b7e514d03de835ef56f1a
Dave Paul <david.paul@showtime.net>
2018-03-02 14:15:16 -0500

    wired up redux todos example

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,3 +1,15 @@
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
 
-render(<h1>Hello frickin world!</h1>, document.getElementById('root'));
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
