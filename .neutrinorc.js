285cee6e27dd5519b653b64d656cc0c141c27e4b
Dave Paul <david.paul@showtime.net>
2018-03-06 16:25:04 -0500

    set absolute base href to fix asset refs in links other than initial route

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,13 +1,16 @@
 // .neutrinorc.js
 module.exports = {
+  options: {
+    source: 'app/javascript'
+  },
   use: [
     ['@neutrinojs/react', {
       html: {
-        title: 'asbo'
+        title: 'asbo',
+        // fix for htmlHistoryFailsafe
+        // this makes index.js load on any url, ie for reloading
+        baseHref : '/' 
       }
     }]
-  ],
-  options: {
-    source: 'app/javascript'
-  }
+  ]
 }
