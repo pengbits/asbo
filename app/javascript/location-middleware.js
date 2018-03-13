183acd38397eddc384b8b833f3eee072652a3a7e
Dave Paul <david.paul@showtime.net>
2018-03-13 11:31:51 -0400

    redirects

--- app/javascript/location-middleware.js
+++ app/javascript/location-middleware.js
@@ -1,24 +1,17 @@
 import {push} from 'react-router-redux'
 import routes from './routes'
 import {
-  LOAD_PLATFORMS,
-  loadPlatforms,
-  LOAD_PLATFORM,
-  loadPlatform,
-  NEW_PLATFORM,
-  newPlatform
+  LOAD_PLATFORMS, loadPlatforms,
+  LOAD_PLATFORM,  loadPlatform,
+  NEW_PLATFORM,   newPlatform,
+  CREATE_PLATFORM,
 } from './redux/platforms'
 
 const LocationMiddleware = store => next => action => {
-  // dispatch the action after a tiny delay, ie on next render
-  // const deferredDispatch = (action, delay=0) => {
-  //   return setTimeout(() => store.dispatch(action), delay)
-  // }
+
   if(typeof action =='object'){ // not true of thunks, they'll be functions
     if(action.type == '@@router/LOCATION_CHANGE'){
       
-      // @todo extract these regexs into some kidn of map,
-      // ideally one that could be exposed to the <route> composition on index.js
       const actionPath  = (action.payload || {}).pathname.replace(/\/$/,'')
         switch(true) {
           case routes.test(actionPath, LOAD_PLATFORMS):
@@ -33,8 +26,11 @@ const LocationMiddleware = store => next => action => {
             const nickname = routes.params(actionPath, LOAD_PLATFORM, 'nickname')
             store.dispatch(loadPlatform({nickname}))
             break
-            
         }
+    }
+    // this behaves like a typical rails-style redirect... just need a flash mesage?
+    if(action.type == `${CREATE_PLATFORM}_FULFILLED`){
+      store.dispatch(push('/platforms'))
     }
   }
   return next(action)
