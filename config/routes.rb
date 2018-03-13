189ab65a0789db603fa41c568d88b180fab8958c
Dave Paul <david.paul@showtime.net>
2018-03-13 16:03:02 -0400

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-   get 'platforms', to: 'platforms#index'
-  post 'platforms', to: 'platforms#create'
+   get 'platforms', to: 'platforms#index' 
+  post 'platforms', to: 'platforms#create', as: 'create_platform'
   
    get 'platforms/:nickname', to: 'platforms#show', as: 'platform'
+delete 'platforms/:nickname', to: 'platforms#destroy', as: 'delete_platform'
+
   # this should prolly be PUT or POST since it creates episodes but platform exists
    get 'platforms/:nickname/refresh', to: 'platforms#refresh', as: 'platform_refresh'
    
