eac69edd8cf568315974b07eac8dc1eb340ffda3
Dave Paul <david.paul@showtime.net>
2018-03-20 13:40:50 -0400

    babel

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,3 @@
-console.log('hello world')
+const wibble = {'foo':true,'bar':false}
+const {foo,bar} = wibble;
+console.log(`${foo} / ${bar}`)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const paths = {
   SRC:  path.resolve(__dirname, 'src'),
   JS:   path.resolve(__dirname, 'src/js'),
 };
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Import our plugin -> ADDED IN THIS STEP
 
 // Webpack configuration
 module.exports = {
@@ -14,7 +15,35 @@ module.exports = {
     path: paths.DIST,
     filename: 'app.bundle.js'
   },
-  devServer: {
-    contentBase: paths.SRC,
+  // devServer: {
+  //   contentBase: paths.SRC,
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html')
+    })
+  ],
+  // Loaders configuration -> ADDED IN THIS STEP
+  // We are telling webpack to use "babel-loader" for .js and .jsx files
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  // Enable importing JS files without specifying their's extenstion -> ADDED IN THIS STEP
+  //
+  // So we can write:
+  // import MyComponent from './my-component';
+  //
+  // Instead of:
+  // import MyComponent from './my-component.jsx';
+  resolve: {
+    extensions: ['.js', '.jsx'],
   }
 };
