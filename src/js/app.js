e3b237d0f1ff8ca3dba57719709d3a5572b6c397
Dave Paul <david.paul@showtime.net>
2018-03-20 14:03:58 -0400

    loading images

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import '../scss/style.scss';
+import logo from '../logo.svg'
 
 export default class Hello extends Component {
   render() {
     return (
       <div>
         Hello from react
+        
+        <img src={ logo } alt='showteeezy ' />
       </div>
     );
   }
