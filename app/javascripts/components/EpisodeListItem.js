8512b42ae22bf0bfba254afebf9fb5ddcf5f36a7
Dave Paul <david.paul@showtime.net>
2018-03-23 14:33:22 -0400

    styling the episode list item

--- app/javascripts/components/EpisodeListItem.js
+++ app/javascripts/components/EpisodeListItem.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 const EpisodeListItem = ({id,name}) => (<li className='menu__item'>
-  {name}
+  <span className='episode-snap'>
+    <Link className='episode-snap__title' to={`/episodes/${id}`}>{name}</Link>
+  </span>
 </li>)
 
 export default EpisodeListItem
