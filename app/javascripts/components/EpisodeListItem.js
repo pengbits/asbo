6795eef86eee42991c9db6ae8606b15ea124e343
Dave Paul <david.paul@showtime.net>
2018-03-27 11:28:58 -0400

    platform pill w/ hardcoded `rinse`

--- app/javascripts/components/EpisodeListItem.js
+++ app/javascripts/components/EpisodeListItem.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
-const EpisodeListItem = ({id,name}) => (<li className='menu__item'>
-  <Link to={`/episodes/${id}`}>{name}</Link>
-</li>)
+const EpisodeListItem = ({id,name,platformName}) => (
+  <li className='menu__item'>
+    <Link to={`/episodes/${id}`}>{name}</Link>
+    <em className='menu__item-meta'>
+      rinse
+    </em>
+  </li>
+)
 
 export default EpisodeListItem
