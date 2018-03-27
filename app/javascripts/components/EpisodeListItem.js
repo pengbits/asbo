0ddefe5972d6e0a62ece737a11ed2c8215d19694
Dave Paul <david.paul@showtime.net>
2018-03-27 14:12:11 -0400

    reverted platform node of episodes list

--- app/javascripts/components/EpisodeListItem.js
+++ app/javascripts/components/EpisodeListItem.js
@@ -1,13 +1,9 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
-const EpisodeListItem = ({id,name,platform}) => (
+const EpisodeListItem = ({id,name}) => (
   <li className='menu__item'>
     <Link to={`/episodes/${id}`}>{name}</Link>
-    {platform && 
-      <span className='menu__item__meta'>
-        <em className='flag'>{platform.nickname}</em>
-      </span>}
   </li>
 )
 
