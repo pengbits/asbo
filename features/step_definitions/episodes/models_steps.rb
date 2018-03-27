340bf78f2f2764264076a29f86d18b3d876b449c
Dave Paul <david.paul@showtime.net>
2018-03-27 11:03:52 -0400

    mv step def for platform.read

--- features/step_definitions/episodes/models_steps.rb
+++ features/step_definitions/episodes/models_steps.rb
@@ -6,8 +6,8 @@ end
 When(/I load the episode endpoint with a (good|bad) id/) do |mode|
   i   = rand(@episodes.length)-1
   @ep = @episodes[i]
-  id  = mode == 'bad' ? 1 : @ep.id
-  url = "/episodes/#{id}"
+  @id = mode == 'bad' ? 1 : @ep.id
+  url = "/episodes/#{@id}"
   puts "GET #{url}"
   get url
   @response = last_response.body
@@ -15,4 +15,10 @@ end
 
 Then("the response should be a JSON representation of the episode") do
   expect(@response).to include(@ep.to_json)
+end
+
+Then("the response should be an episode not found error") do
+  expect(last_response.status).to eq(500)
+  error_json = {"error" => "Couldn't find Episode with 'id'=#{@id}"}.to_json
+  expect(@response).to eq(error_json)
 end
