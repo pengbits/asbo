e5056f61000ecc1de87cee6498712be0cd5618fc
Dave Paul <david.paul@showtime.net>
2018-03-29 16:55:56 -0400

    more grid styles

--- app/javascripts/components/EpisodeGridItem.js
+++ app/javascripts/components/EpisodeGridItem.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
-const EpisodeGridItem = ({id,name,date,platform}) => (
-  <div className='grid__item'>
+const bg = ({image}) => {
+  return {
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundImage:`url(${image})`
+  }
+}
+
+const EpisodeGridItem = ({id,name,image,date,platform}) => (
+  <div className='grid__item' style={bg({image})}>
+    <span className='grid__mask'>
+    </span>
     <div className='episode-tile'>
       <Link className='episode-tile__title' to={`/episodes/${id}`}>{name}</Link>
       <span className='episode-tile__date'>{date}</span>
