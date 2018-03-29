806fffbd9cbe1d53c8102cabffdc311670dcc3c5
Dave Paul <david.paul@showtime.net>
2018-03-29 13:50:46 -0400

    wiring platform steps

--- features/step_definitions/episodes/image_steps.rb
+++ features/step_definitions/episodes/image_steps.rb
@@ -4,12 +4,9 @@ Given("the platform has relative_image_paths={string}") do |mode|
 end
 
 Then("the platform will contain an episode with this image {string}") do |src|
+  puts "expecting #{src}"
   @ep = @platform.episodes.find do |ep|
     ep.image == src
   end
   expect(@ep).to be_truthy
-end
-
-Then("the episode will have this fallback image {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
 end
