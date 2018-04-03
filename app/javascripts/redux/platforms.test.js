87e701b067fe3fc9b0090df30f01980fdb896562
Dave Paul <david.paul@showtime.net>
2018-04-03 11:08:16 -0400

    asserting against async actions with thunk and promise middleware in mock store

--- app/javascripts/redux/platforms.test.js
+++ app/javascripts/redux/platforms.test.js
@@ -1,16 +1,24 @@
+// mock store setup
+import promiseMiddleware from 'redux-promise-middleware'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+const middlewares = [promiseMiddleware(),thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares)
+
+// load application code & mock the api
 import * as p from './platforms';
+const reducer = p.reducer;
 import API from '../api'
 jest.mock('../api')
 
-const reducer = p.reducer;
-
+// begin tests
 describe('Platforms', () => {
-  describe('platforms#index', () => {
-    test('there are no platforms in list at startup', () => {
+  describe('platforms#index json', () => {
+    it('is empty at startup', () => {
       expect(p.initialState.platforms).toHaveLength(0)
     })
     
-    test('there are some platforms in list after fetch', async () => {
+    it('contains some platforms in list after fetch', async () => {
       const data = await API.getPlatforms()
       expect(data).toHaveLength(3)
       // const {platforms} = await
@@ -20,10 +28,19 @@ describe('Platforms', () => {
     })
   })
   
-  describe('platforms#show', () => {
-    test('returns a valid platform', async () => {
-      const data = await API.getPlatform({nickname:'nts'})
-      expect(data).toBeTruthy()
+  describe('platforms#show js', () => {
+    it('contains a valid platform', async () => {
+      const store = mockStore({})
+      const action = p.loadPlatform({'nickname':'nts'});
+      return store.dispatch(action)
+        .then(() => {
+            const actions = store.getActions()
+            expect(actions).toHaveLength(2)
+            expect(actions.map(a => a.type)).toEqual([
+              "LOAD_PLATFORM_PENDING",
+              "LOAD_PLATFORM_FULFILLED"
+            ])
+        })
     })
   })
 })
