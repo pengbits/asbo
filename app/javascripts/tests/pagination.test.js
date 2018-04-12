61606ae1a76e16133f54b3240bfed82c8acf32a8
Dave Paul <david.paul@showtime.net>
2018-04-12 15:29:00 -0400

    kind of a mess

--- app/javascripts/tests/pagination.test.js
+++ app/javascripts/tests/pagination.test.js
@@ -1,3 +1,6 @@
+jest.mock('../api')
+import mockStore from './mockStore' ;
+
 import {
   pagination,
   requestPage, REQUEST_PAGE,
@@ -5,9 +8,24 @@ import {
 } from '../redux/pagination'
 
 describe('Pagination', () => {
+  
   describe('currentPage', () => {
     it('is set to 1 at init', () => {
       expect(reducer().currentPage).toBe(1)
+    })
+  })
+  
+  describe('pages', () => {
+    it('can responds to requestPage action', async () => {
+      const store = mockStore()
+      await store.dispatch(requestPage({
+        page: 2,
+        resource: 'platform',
+        nickname: 'rinse'
+      }))
+        .then(() => {
+          console.log('page back')
+        })
     })
   })
 })
