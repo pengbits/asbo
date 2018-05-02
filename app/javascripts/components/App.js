7c3346864e61593c5a19f929c1ff0d9191caba07
Dave Paul <david.paul@showtime.net>
2018-05-02 15:02:01 -0400

    carving out space for media player

--- app/javascripts/components/App.js
+++ app/javascripts/components/App.js
@@ -5,9 +5,14 @@ import '../../stylesheets/app.scss'
 const App = ({children}) => (
   <div className="container">
     <div className="content">
-      <h1 className='h1'>Asbo</h1>
-      <Nav />
-      {children}
+      <div className="content__primary">
+        <h1 className='h1'>Asbo</h1>
+        <Nav />
+        {children}
+      </div>
+      <div className="content__secondary">
+        <p>!</p>
+      </div>
     </div>
   </div>
 )
