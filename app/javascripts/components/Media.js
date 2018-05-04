6dcc512bd39914a1eb213def2ea27589a65ccc95
Dave Paul <david.paul@showtime.net>
2018-05-04 16:30:42 -0400

    comments

--- app/javascripts/components/Media.js
+++ app/javascripts/components/Media.js
@@ -9,6 +9,8 @@ class Media extends Component {
     } = this.props
     
     // obv kinda shaky to just dump the snippet from a 3rd party api into our app
+    // perhaps just use soundcloud's oembed api to coerce the url into a track id,
+    // and build up the template ourselves? and what about mixcloud?
     if(embed && embed.html){
       return <div dangerouslySetInnerHTML={{__html: embed.html}} />
     } else {
