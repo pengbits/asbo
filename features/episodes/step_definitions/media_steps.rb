4f94af564b732d7fa52cdcf0c86f644e436d42b7
Dave Paul <david.paul@showtime.net>
2018-05-07 15:29:25 -0400

    sktching mixcloud tests

--- features/episodes/step_definitions/media_steps.rb
+++ features/episodes/step_definitions/media_steps.rb
@@ -4,32 +4,24 @@ Then("the platform will contain an episode with this media property") do |media|
   expect(@ep.media).to eq(media.hashes.first)
 end
 
-Given("I have a media object with a url and a valid type") do
+Given("I have a media object") do |attrs|
   @media = {
-    media: {
-      url: "https://soundcloud.com/rinsefm/theheatwave020518",
-      type: "soundcloud"
-    }
-  }
-end
-
-Given("I have a media object with a url and an invalid type") do
-  @media = {
-    media: {
-      type: "foo"
-    }
+    media: JSON.parse(attrs)
   }
 end
 
 When("I post the attributes to the media endpoint") do
   post "/media", @media
   @response = last_response.body
+  puts @response
   @json = JSON.parse(@response)
 end
 
-Then("I will get an embed back") do
+Then("I will get this embed back") do |html|
   # pending # Write code here that turns the phrase above into concrete actions
+  puts @json['html']
   expect(@json['html']).to be_truthy
+  expect(@json['html']).to eq(html)
 end
 
 Then("I will get an error") do
