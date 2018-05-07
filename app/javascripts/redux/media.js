7b1ce69e43a6deffee7c191d0fa653a0fd050287
Dave Paul <david.paul@showtime.net>
2018-05-07 13:53:45 -0400

    edits

--- app/javascripts/redux/media.js
+++ app/javascripts/redux/media.js
@@ -5,20 +5,17 @@ import {createAction,createActions,handleActions} from 'redux-actions'
 // constants
 export const INIT_MEDIA = 'INIT_MEDIA'
 export const FETCH_EMBED = 'FETCH_EMBED'
-export const PLAY_MEDIA = 'PLAY_MEDIA'
-export const STOP_MEDIA = 'PLAY_MEDIA'
+export const CLEAR_MEDIA = 'CLEAR_MEDIA'
 
 export const initialState = {
   url : null,
-  isPlaying : false,
   type: null,
   embed: null
 }
 
 
 export const initMedia = createAction(INIT_MEDIA)
-export const playMedia = createAction(PLAY_MEDIA)
-export const stopMedia = createAction(STOP_MEDIA)
+export const clearMedia = createAction(CLEAR_MEDIA)
 
 export const fetchEmbed = function({url,type}){
   return {
@@ -37,17 +34,9 @@ export const reducer = function(state=initialState, action={}){
         type,
         ...state
       }
-    case PLAY_MEDIA:
-      return {
-        isPlaying: true,
-        ...state
-      }
-      
-    case STOP_MEDIA:
-      return {
-        isPlaying: false,
-        ...state
-      }
+    
+    case CLEAR_MEDIA:
+      return initialState
       
     case `${FETCH_EMBED}_FULFILLED`:
       let {html} = action.payload
